1a11bdf8a54428598c110e64634f9a5e9971ce82
Ciro S. Costa <ciro9758@gmail.com>
2015-05-18 15:04:02 -0300

    adds initial gmp code, adds editorconfig

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,58 @@
 #include <iomanip>
 #include <iostream>
+#include <pthread.h>
+#include <semaphore.h>
 #include <math.h>
-#include <gmpxx.h>
+#include <gmp.h>
 
-//TODO introduce gmp
 //TODO 'usage' print and settings helper
+
 //TODO get cpu cores
+
 //TODO implement butterfly barrier as well as
-//     other types
+//     other types to compare
 
-using std::cout;
-using std::endl;
+//TODO learn how to measure execution time of
+//     different parts of out code with a great
+//     precision.
 
-double factorial(int n)
+
+void compute(mpf_t result, int n, mpf_t x)
 {
-    double res = n;
-    if (!n)
-        return 1;
+  mpf_t fact2n, lres;
 
-    while (--n)
-        res *= n;
+  mpf_init_set_ui(fact2n, 1);
+  mpf_init_set_ui(lres, 0);
 
-    return res;
+  for (int i = 2; i <= 2*n; i++)
+    mpf_mul_ui(fact2n, fact2n, i);
+
+  mpf_pow_ui(lres, x, 2*n);
+  mpf_div(lres, lres, fact2n);
+
+  if (n%2)
+    mpf_sub(result, result, lres);
+  else
+    mpf_add(result, result, lres);
+
+  mpf_clear(lres);
+  mpf_clear(fact2n);
 }
 
 int main() {
-    long double result = 0.0;
-    int x = 1;
+  mpf_set_default_prec(1000);
+  mpf_t result, x;
 
-    for (int i = 0; i < 10; i++) {
-        if (i%2)
-            result -= (pow(x, 2*i))/(factorial(2*i));
-        else
-            result += (pow(x, 2*i))/(factorial(2*i));
-    }
+  mpf_init_set_ui(result, 0);
+  mpf_init_set_ui(x, 1);
 
-    cout << std::setprecision(100) << result << endl;
+  for (int i = 0; i < 1000; i++)
+    compute(result, i, x);
 
-    return 0;
+  mpf_out_str(stdout, 10, 0, result);
+
+  mpf_clear(result);
+  mpf_clear(x);
+
+  return 0;
 }
